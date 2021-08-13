1b96408fdbbec2e57eb8092b159294ddc08dc8df
Javier Santana <javiersantanagodoy@gmail.com>
2021-08-13 01:04:59 +0100

    Added cleaner method for delete the downloaded files

--- main.py
+++ main.py
@@ -52,14 +52,42 @@ def download():
             errors.append(filename)
     print_results(lines,errors)
 
+def removePdfFiles():
+    for parent, dirnames, filenames in os.walk('.'):
+        for fn in filenames:
+            if fn.lower().endswith('.pdf'):
+                os.remove(os.path.join(fn))
+
+def countPdfFiles():
+    counter = 0
+    for parent, dirnames, filenames in os.walk('.'):
+        for fn in filenames:
+            if fn.lower().endswith('.pdf'):
+                counter = counter + 1
+    return counter
+
+def clean():
+    clean = input("Done! Do you want to clean the downloaded files? (y/n)")
+    if not clean.lower() == "n":
+        print("Okay! Using the Roomba...")
+        removePdfFiles()
+        if (countPdfFiles() == 0):
+            print("Done! All clean for tomorrow!")
+        else:
+            print("Delete error, some files are still in the folder. Please check")
+    else:
+        print("No problem! Have a nice day!")
+
 
 
 def main():
     download()
+    clean()
 
 # Config zone
 telegram_token = AlldebridConfig.telegram_token
 channel_id = AlldebridConfig.channel_id
 downloads_path = AlldebridConfig.downloads_path
+lines = ""
 
 main()
