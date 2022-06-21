23a1d8b8af71b6836c537012ec18adc97662c4b7
Javier Santana <javiersantanagodoy@gmail.com>
2022-06-22 00:12:55 +0100

    Adapt config template to using ID and not URL

--- resources/config.py
+++ resources/config.py
@@ -10,13 +10,13 @@ class TelegramApi(object):
     api_id = ''
     api_hash = ''
     phone_number = ''
-    source_chat_url = ''
+    source_chat_id = ''
     source_chat_name = ''
     source_chat_limit = 0
-    newspapers_chat_url = ''
+    newspapers_chat_id = ''
     newspapers_chat_name = ''
     newspapers_chat_limit = 0
-    magazines_chat_url = ''
+    magazines_chat_id = ''
     magazines_chat_name = ''
     magazines_chat_limit = 0
     admin_alias = ''
